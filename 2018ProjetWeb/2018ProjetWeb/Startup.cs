daa889b0850084754019f892037b898d247c9559
Hildisum <hildisum@gmail.com>
2018-05-07 22:46:10 +0200

    Initiliation Projet Web 2018

---
+++ 2018ProjetWeb/2018ProjetWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2018ProjetWeb.Startup))]
+namespace _2018ProjetWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
